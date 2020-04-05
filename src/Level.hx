2d99c98ed1f4d561dfe09011d1df39198015747b
Stuart Thomson <stuart.thomson@vships.com>
2020-04-05 05:48:37 +0100

    Cool Fireball - code reworked

--- src/Level.hx
+++ src/Level.hx
@@ -2,77 +2,55 @@ class Level extends dn.Process {
 	public var game(get,never) : Game; inline function get_game() return Game.ME;
 	public var fx(get,never) : Fx; inline function get_fx() return Game.ME.fx;
 
-	public var wid(get,never) : Int; inline function get_wid() return 4;
-	public var hei(get,never) : Int; inline function get_hei() return 4;
+	public var wid(get,never) : Int; inline function get_wid() return 16;
+	public var hei(get,never) : Int; inline function get_hei() return 16;
 
-	var starx = 443.0;
-	var speed2 = 643.0;
-	var speed3 = -143.0;
+	var hcount = 0;
+ 
+	var xspeed = 555.0;
+	//var yheight : Array<Float> = [];
+	var yheight = 0;
 
 	var invalidated = true;
-
-	var stars : Array<Float> = [];
-
-	var starspeed : Array<Float> = [];
 
 	public function new() {
 		super(Game.ME);
 		createRootInLayers(Game.ME.scroller, Const.DP_BG);
-
-		for(counter in 0...44) {
-
-			var yy = Math.random() * 433;
-			stars[counter] = yy;
-		}
-		
-		for(counterspeed in 0...44) {
-			var sv = Math.random() * 0.3;
-			starspeed[counterspeed] = 3 ;
-		}
 	}
 
 	public inline function isValid(cx,cy) return cx>=0 && cx<wid && cy>=0 && cy<hei;
 	public inline function coordId(cx,cy) return cx + cy*wid;
 
-	// 3 simple background 'stars'
 
 	public function render() {
-
+		// Debug level render
 		root.removeChildren();
+		// for(cx in 0...wid)
+		// for(cy in 0...hei) {
+		// 	var g = new h2d.Graphics(root);
+		// 	g.beginFill(Color.randomColor(rnd(0,1), 0.5, 0.4), 1);
+		// 	g.drawRect(cx*Const.GRID, cy*Const.GRID, Const.GRID, Const.GRID);
+		// }
 
-		if(starx < -900) {
-			starx = 900;
+		if(xspeed < -900) {
+			xspeed = 900;
 		}
 
-		if(speed2 < -900) {
-			speed2 = 900;
-		}
-
-		if(speed3 < -900) {
-			speed3 = 900;
-		}
-			
-		starx += -0.1*tmod*22;
-		//fx.lightSpot(starx, 88, 0xffffff, 0.04);
-
-		speed2 += -0.1*tmod*44;
-		//fx.lightSpot(speed2, 33, 0x0000ff, 0.02);
-
-		speed3 += -0.1*tmod*14;
-		//fx.fxsmallcircle(speed3, -83, 0xff00ff, 0.05);
-
-		speed3 += -0.1*tmod*74;
+		xspeed += -0.1*tmod*74;
 
 		// cool
-		var coolh = Math.random() * 0.3 * 100;
+		var yheight = Math.random() * 0.3 * 100;
 
-		fx.fxsmallcircle(speed3, coolh , 0x0000ff, 1);
+		fx.fxsmallcircle(xspeed, yheight , 0x0000ff, 1);
 
 		fx.lightSpot(
-			speed3,coolh,
+			xspeed,
+			yheight,
 			Color.interpolateInt(0xff0000,0xffcc00,rnd(0,1)),
 			0.2
 		);
+
+
 	}
 
 	override function postUpdate() {
@@ -84,4 +62,3 @@ class Level extends dn.Process {
 		}
 	}
 }
-
