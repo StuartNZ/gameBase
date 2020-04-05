3b7cbcbb5b9e45f01b1098f08ad0cd645561066d
Stuart Thomson <stuart.thomson@vships.com>
2020-04-05 07:57:05 +0100

    Fireball Storm 2 - implemented minimum speed and range

--- src/Level.hx
+++ src/Level.hx
@@ -5,17 +5,14 @@ class Level extends dn.Process {
 	public var wid(get,never) : Int; inline function get_wid() return 16;
 	public var hei(get,never) : Int; inline function get_hei() return 16;
 
-	var hcount = 0;
- 
-	var speed = 0.0;
-	//var yheight : Array<Float> = [];
-	var yheight = 0;
-
 	var stars : Array<Star> = [];
 
 	var invalidated = true;
 
 	var starcount = 10;
+
+	var min_speed = 30;
+	var speed_range = 70;
 
 	public function new() {
 		super(Game.ME);
@@ -35,40 +32,21 @@ class Level extends dn.Process {
 		// Debug level render
 		root.removeChildren();
 
-		// reset x and y 
-		// if(speed < -900) {
-		// 	speed = 900;
-		// 	yheight = -400 + Std.random(800); 
-		// }
-
-
 		for(counter in 0...starcount) {
 			if(stars[counter].x < -900) {
+				// reset 
 				stars[counter].x = 900;
-				// reset height
 				stars[counter].y = -100 + Std.random(400);
+				stars[counter].speed = min_speed + Std.random(speed_range);
 			}
 	
 		}
 
-
-
-		//xspeed += -0.1*tmod*174;
-
-		//RenderFireball(xspeed, yheight);
-
-		//speed += -0.1*tmod*5;
-
 		for(counter in 0...starcount) {
-			//stars[counter] = new Star(Std.random(80),-300+Std.random(300));
 			stars[counter].x += -0.1*tmod*stars[counter].speed;
-
-
 			RenderFireball(stars[counter].x, stars[counter].y);
 		}
 
-
-	
 	}
 
 	public function RenderFireball(x:Float, y:Float){
@@ -98,16 +76,10 @@ class Star {
 	public var y:Float;
 	public var speed:Float;
 
-
-	// publci function new() {
-	// 	x = Std.random(50);
-	// 	y = -300 + Std.random(600);
-	// }
-
 	public function new(x,y) {
 		this.x = x;
 		this.y = y;
 
-		this.speed = Std.random(140);
+		this.speed = 30 + Std.random(70);
 	}
 }
