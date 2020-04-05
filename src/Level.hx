de4c394c7147eb6d121badc55f3b6ccf075080c1
Stuart Thomson <stuart.thomson@vships.com>
2020-04-05 07:47:27 +0100

    Fireball Storm

--- src/Level.hx
+++ src/Level.hx
@@ -7,15 +7,24 @@ class Level extends dn.Process {
 
 	var hcount = 0;
  
-	var xspeed = 555.0;
+	var speed = 0.0;
 	//var yheight : Array<Float> = [];
 	var yheight = 0;
 
+	var stars : Array<Star> = [];
+
 	var invalidated = true;
+
+	var starcount = 10;
 
 	public function new() {
 		super(Game.ME);
 		createRootInLayers(Game.ME.scroller, Const.DP_BG);
+
+		for(counter in 0...starcount) {
+			stars[counter] = new Star(-400+Std.random(480),-100+Std.random(300));
+		}
+
 	}
 
 	public inline function isValid(cx,cy) return cx>=0 && cx<wid && cy>=0 && cy<hei;
@@ -25,33 +34,54 @@ class Level extends dn.Process {
 	public function render() {
 		// Debug level render
 		root.removeChildren();
-		// for(cx in 0...wid)
-		// for(cy in 0...hei) {
-		// 	var g = new h2d.Graphics(root);
-		// 	g.beginFill(Color.randomColor(rnd(0,1), 0.5, 0.4), 1);
-		// 	g.drawRect(cx*Const.GRID, cy*Const.GRID, Const.GRID, Const.GRID);
+
+		// reset x and y 
+		// if(speed < -900) {
+		// 	speed = 900;
+		// 	yheight = -400 + Std.random(800); 
 		// }
 
-		if(xspeed < -900) {
-			xspeed = 900;
+
+		for(counter in 0...starcount) {
+			if(stars[counter].x < -900) {
+				stars[counter].x = 900;
+				// reset height
+				stars[counter].y = -100 + Std.random(400);
+			}
+	
 		}
 
-		xspeed += -0.1*tmod*74;
-
-		// cool
-		var yheight = Math.random() * 0.3 * 100;
-
-		fx.fxsmallcircle(xspeed, yheight , 0x0000ff, 1);
-
-		fx.lightSpot(
-			xspeed,
-			yheight,
-			Color.interpolateInt(0xff0000,0xffcc00,rnd(0,1)),
-			0.2
-		);
 
 
+		//xspeed += -0.1*tmod*174;
+
+		//RenderFireball(xspeed, yheight);
+
+		//speed += -0.1*tmod*5;
+
+		for(counter in 0...starcount) {
+			//stars[counter] = new Star(Std.random(80),-300+Std.random(300));
+			stars[counter].x += -0.1*tmod*stars[counter].speed;
+
+
+			RenderFireball(stars[counter].x, stars[counter].y);
+		}
+
+
+	
 	}
+
+	public function RenderFireball(x:Float, y:Float){
+			fx.fxsmallcircle(x, y, 0x0000ff, 1);
+
+			fx.lightSpot(
+				x,
+				y,
+				Color.interpolateInt(0xff0000,0xffcc00,rnd(0,1)),
+				0.2
+			);
+
+		}
 
 	override function postUpdate() {
 		super.postUpdate();
@@ -60,5 +90,24 @@ class Level extends dn.Process {
 			//invalidated = false;
 			render();
 		}
+	}
+}
+
+class Star {
+	public var x:Float;
+	public var y:Float;
+	public var speed:Float;
+
+
+	// publci function new() {
+	// 	x = Std.random(50);
+	// 	y = -300 + Std.random(600);
+	// }
+
+	public function new(x,y) {
+		this.x = x;
+		this.y = y;
+
+		this.speed = Std.random(140);
 	}
 }
